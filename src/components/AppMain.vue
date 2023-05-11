daff7bb70d0065fc433c849b4b727b1b663e8b4f
Luis-Mocha <luismocha97@gmail.com>
2023-05-11 13:06:25 +0200

    eliminazion comps "popular" e rispettivi singoli

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,14 +2,10 @@
 import axios from 'axios';
 import { store } from '../store';
 import SingleCard from './SingleCard.vue';
-import PopularMovies from './PopularMovies.vue';
-import PopularTv from './PopularTv.vue';
 
 export default {
     name: 'AppMain',
     components: {
-        PopularMovies,
-        PopularTv,
         SingleCard,
     },
     data() {
@@ -18,80 +14,26 @@ export default {
         }
     },
     created() {
-        this.apiPopularMovies()
-        this.apiPopularTv()
+        this.apiGetPopular()
     },
     methods : {
 
-        // modifico i codici lingua non riconosciuti dall'api per le bandiere
-        changeFlag(par) {
-            if (par.original_language === 'en') {
-                par.original_language = 'gb'
-            }
-            else if (par.original_language === 'ko') {
-                par.original_language = 'kr'
-            }
-            else if (par.original_language === 'da') {
-                par.original_language = 'dk'
-            }
-            else if (par.original_language === 'ja') {
-                par.original_language = 'jp'
-            };
-        },
-
-        //api film popolari
-        apiPopularMovies() {
-            let infoMovies;
-            
-            axios.get( `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
+        apiGetPopular() {
+            // Movies
+            axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
+            // axios.get( `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
                 .then( (res) => {
-                console.log(res.data.results) 
-                
-                infoMovies = res.data.results;
+                console.log(res.data)
 
-                infoMovies.forEach(element => {
-                    // modifico i codici lingua non riconosciuti dall'api per le bandiere
-                    this.changeFlag(element)
-
-                    // moddifico le date
-                    const newFormat = element.release_date.split('-');
-                    element.release_date = `${newFormat[2]}-${newFormat[1]}-${newFormat[0]}`;
-
-                    //Modifico i voti
-                    const newVote = Math.ceil((element.vote_average  * 5 / 10));
-                    element.vote_average = newVote;
-
-                })
-                store.arrayPopularMovies = infoMovies;
-                console.log(store.arrayPopularMovies);
+                store.arrayPopularMovies = res.data;
             });
-        },
-
-        // serie tv popolari
-        apiPopularTv() {
-            let infoMovies;
             
-            axios.get( `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
+            // Tv series
+            axios.get(`https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
                 .then( (res) => {
-                console.log(res.data.results) 
-                
-                infoMovies = res.data.results;
+                console.log(res.data)
 
-                infoMovies.forEach(element => {
-                    // modifico i codici lingua non riconosciuti dall'api per le bandiere
-                    this.changeFlag(element)
-
-                    // moddifico le date
-                    const newFormat = element.first_air_date.split('-');
-                    element.first_air_date = `${newFormat[2]}-${newFormat[1]}-${newFormat[0]}`;
-
-                    //Modifico i voti
-                    const newVote = Math.ceil((element.vote_average  * 5 / 10));
-                    element.vote_average = newVote;
-
-                })
-                store.arrayPopularTv = infoMovies;
-                console.log(store.arrayPopularTv);
+                store.arrayPopularTv = res.data;
             });
         },
 
@@ -255,7 +197,6 @@ export default {
                     <i class="fa-solid fa-angle-right" @click="nextPageMovie('movie', store.arrayMovies)"></i>
                 </span>
             </div>
-            
 
             <div class="slider"> 
                 <SingleCard
@@ -281,7 +222,6 @@ export default {
                     <i class="fa-solid fa-angle-right" @click="nextPageTv('tv', store.arrayTv)"></i>
                 </span>
             </div>
-            
 
             <div class="slider"> 
                 <SingleCard
@@ -291,9 +231,31 @@ export default {
             </div>
         </div>
 
-        <PopularMovies/>
+        <div id="popMovieSection" class="main-section">
+            <h2>
+                Popular Movies
+            </h2>
 
-        <PopularTv/>
+            <div class="slider"> 
+                <SingleCard
+                v-for="(elem, index) in store.arrayPopularMovies.results" :key="index"
+                :info="elem"
+                />
+            </div>
+        </div>
+
+        <div id="popTvSection" class="main-section">
+            <h2>
+                Popular TvSeries
+            </h2>
+
+            <div class="slider"> 
+                <SingleCard
+                v-for="(elem, index) in store.arrayPopularTv.results" :key="index"
+                :info="elem"
+                />
+            </div>
+        </div>
 
     </main>
     
--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -15,7 +15,6 @@ export default {
             // Movies
             if (store.searchValue !== '') {
                 axios.get( `https://api.themoviedb.org/3/search/movie?${store.ApiPath}&query=${store.searchValue}&language=it_IT`)
-                // axios.get( `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&${store.ApiPath}`)
                     .then( (res) => {
                     console.log(res.data)
 
